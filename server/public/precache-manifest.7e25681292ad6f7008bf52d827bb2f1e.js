1e3763416e3b536b213f4d428df990a74214a845
aswin pyakurel <apyakurel@gmail.com>
2019-05-15 18:09:23 +0200

    adds public files

---
+++ server/public/precache-manifest.7e25681292ad6f7008bf52d827bb2f1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4353710b9fd4718d03b762e7839474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa84cee243e558fe87d7",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b677558a1df20efc00ef",
+    "url": "/static/css/main.a157ce1e.chunk.css"
+  },
+  {
+    "revision": "aa84cee243e558fe87d7",
+    "url": "/static/js/2.a09afca6.chunk.js"
+  },
+  {
+    "revision": "b677558a1df20efc00ef",
+    "url": "/static/js/main.a72b9106.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3dc8cba67211823ad14b8f5b3656149",
+    "url": "/static/media/image.f3dc8cba.png"
+  }
+]);
---
+++ server/public/precache-manifest.7f1b5bc5c020fe3761d4253e0f126a9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55297867779e0b4b2cf7c6762705e2cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa84cee243e558fe87d7",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "070b2fa13cc3f2d10393",
+    "url": "/static/css/main.a157ce1e.chunk.css"
+  },
+  {
+    "revision": "aa84cee243e558fe87d7",
+    "url": "/static/js/2.a09afca6.chunk.js"
+  },
+  {
+    "revision": "070b2fa13cc3f2d10393",
+    "url": "/static/js/main.d08d3c82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3dc8cba67211823ad14b8f5b3656149",
+    "url": "/static/media/image.f3dc8cba.png"
+  }
+]);
